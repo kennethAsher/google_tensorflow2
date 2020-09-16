3bb23d824d590a874e1e3ed1e620c7f2bacbf004
kennethAsher <2573334038@qq.com>
2020-09-16 17:49:56 +0800

    新建回调函数使用方式

---
+++ Keras实战/tf_keras_classification_model_callback.py
@@ -0,0 +1,80 @@
+# !/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+@author : kennethAsher
+@fole   : tf_keras_classification_model.py
+@ctime  : 2020/9/16 13:30
+@Email  : 1131771202@qq.com
+@content: 对比创建标准的分类模型的callback(回调函数)，实现在执行中返回状态
+            在执行过程中会进行各种各样的操作
+"""
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+# %matplotlib inline
+import numpy as np
+import sklearn
+import pandas as pd
+import os
+import sys
+import time
+import tensorflow as tf
+from tensorflow import keras
+
+# 不同物品的数据集
+fashion_mnist = keras.datasets.fashion_mnist
+(x_train_all, y_train_all), (x_test, y_test) = fashion_mnist.load_data()
+x_valid, x_train = x_train_all[:5000], x_train_all[5000:]
+y_valid, y_train = y_train_all[:5000], y_train_all[5000:]
+# 分别获得验证集，训练集，测试集
+print(x_valid.shape, y_valid.shape)
+print(x_train.shape, y_train.shape)
+print(x_test.shape, y_test.shape)
+
+print("这是原始数据")
+print(np.max(x_train), np.min(x_train))
+
+
+class_names = ['T-shirt', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal',
+               'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+# show_imgs(3,5,x_train, y_train, class_names)
+
+# 使用fit_transform: 训练集使用，会记录当前数据集的均值和方差，供以下使用
+# 归一化：（当前值-平均值）/方差 normalize
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+x_train_scaled = scaler.fit_transform(x_train.astype(np.float32).reshape(-1,1)).reshape(-1,28,28)
+x_valid_scaled = scaler.transform(x_valid.astype(np.float32).reshape(-1,1)).reshape(-1,28,28)
+x_test_scaled = scaler.transform(x_test.astype(np.float32).reshape(-1,1)).reshape(-1, 28, 28)
+print("这是归一化之后的数据")
+print(np.max(x_train_scaled), np.min(x_train_scaled))
+
+# 下面来常见一个算法
+model = keras.models.Sequential()
+model.add(keras.layers.Flatten(input_shape=[28, 28]))
+model.add(keras.layers.Dense(300, activation='relu'))
+model.add(keras.layers.Dense(100, activation='relu'))
+model.add(keras.layers.Dense(10, activation='softmax'))
+
+# 当输入y为一个数值的时候，index，使用sparse， 当输入y为列表，例如onehot的时候，不适用sparse
+# optimizer 选择内容很重要
+model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+#需要在训练之前开始执行
+logdir = 'C:\\Users\\GG257\\Desktop\\callback'
+if not os.path.exists(logdir):
+    os.mkdir(logdir)
+output_model = os.path.join(logdir, "fashion_mnist_model.h5")
+callback = [keras.callbacks.TensorBoard(logdir),
+            keras.callbacks.ModelCheckpoint(output_model, save_best_only=True),
+            keras.callbacks.EarlyStopping(patience=5, min_delta=1e-3)]
+
+history = model.fit(x_train_scaled, y_train, epochs=10, validation_data=(x_valid_scaled, y_valid))
+
+
+def plot_learning_curves(history):
+    pd.DataFrame(history.history).plot(figsize=(8, 5))
+    plt.grid(True)
+    plt.gca().set_ylim(0, 1)
+    plt.show()
+
+plot_learning_curves(history)
