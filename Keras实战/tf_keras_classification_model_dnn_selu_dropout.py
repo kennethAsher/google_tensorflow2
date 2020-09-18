6606d403b1c5ec65d92b5fa0e004344321124196
kennethAsher <2573334038@qq.com>
2020-09-18 19:45:41 +0800

    使用Dropout防止过拟合

---
+++ Keras实战/tf_keras_classification_model_dnn_selu_dropout.py
@@ -0,0 +1,75 @@
+# !/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+@author : kennethAsher
+@fole   : tf_keras_classification_model.py
+@ctime  : 2020/9/16 13:30
+@Email  : 1131771202@qq.com
+@content: dropout，防止过拟合的方法
+"""
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+# %matplotlib inline
+import numpy as np
+import sklearn
+import pandas as pd
+import os
+import sys
+import time
+import tensorflow as tf
+from tensorflow import keras
+
+fashion_mnist = keras.datasets.fashion_mnist
+(x_train_all, y_train_all), (x_test, y_test) = fashion_mnist.load_data()
+x_valid, x_train = x_train_all[:5000], x_train_all[5000:]
+y_valid, y_train = y_train_all[:5000], y_train_all[5000:]
+# 查看形状
+print(x_valid.shape, y_valid.shape)
+print(x_train.shape, y_train.shape)
+print(x_test.shape, y_test.shape)
+
+#归一化
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+x_train_scaled = scaler.fit_transform(x_train.astype(np.float32).reshape(-1,1)).reshape(-1,28,28)
+x_valid_scaled = scaler.fit_transform(x_valid.astype(np.float32).reshape(-1,1)).reshape(-1,28,28)
+x_test_scaled = scaler.fit_transform(x_test.astype(np.float32).reshape(-1,1)).reshape(-1,28,28)
+
+#构建模型（dnn，深度学习）
+model = keras.models.Sequential()
+model.add(keras.layers.Flatten(input_shape=[28, 28]))
+for _ in range(20):
+    model.add(keras.layers.Dense(100, activation='selu'))
+model.add(keras.layers.AlphaDropout(rate=0.5))   #rate，丢掉单元的比例是多少
+# model.add(keras.layers.Dropout(rate=0.5))     # 传统的Dropout
+'''
+AlphaDropout效果更好：（使用更多，更适合在批归一化操作中）
+1.均值和方差不变
+2.归一化性质不会变，
+'''
+model.add(keras.layers.Dense(10, activation='softmax'))
+
+#训练模型
+model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+#callbacks(终止返回状态)
+logdir = ('C:\\Users\\GG257\\Desktop\\callback')
+if not os.path.exists(logdir):
+    os.mkdir(logdir)
+output_model_file = os.path.join(logdir,'fashion_mnist_model.h5')
+callbacks = [
+    keras.callbacks.TensorBoard(logdir),
+    keras.callbacks.ModelCheckpoint(output_model_file, save_best_only=True),
+    keras.callbacks.EarlyStopping(patience=5, min_delta=1e-3),
+]
+
+#训练模型
+history = model.fit(x_train_scaled, y_train, epochs=10, validation_data=(x_valid_scaled, y_valid), callbacks=callbacks)
+
+def plot_learning_curves(history):
+    pd.DataFrame(history.history).plot(figsize=(8,5))
+    plt.grid(True)
+    plt.gca().set_ylim(0,1)
+    plt.show()
+plot_learning_curves(history)
