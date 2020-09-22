67321d45dc5a96b5fff302709a9bc870ae2145a6
kennethAsher <2573334038@qq.com>
2020-09-22 11:05:37 +0800

    自定义损失函数操作

---
+++ tensorflow基础API/tf_keras_regression_customized_loss.py
@@ -0,0 +1,9 @@
+# !/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+@author : kennethAsher
+@fole   : tf_keras_regression_customized_loss.py
+@ctime  : 2020/9/22 10:02
+@Email  : 1131771202@qq.com
+@content: 使用自定义api构造一个分类
+"""
